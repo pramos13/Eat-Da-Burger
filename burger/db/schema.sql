9ca560682cc7bcfb6f8d7588cdbf431a200bfb04
Phillip Ramos <phillipramos@Phillips-MacBook-Pro.local>
2016-07-06 10:49:56 -0400

    I modified a few files and completed the config, controllers, and models files

--- burger/db/schema.sql
+++ burger/db/schema.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE burgers_db;
 
 CREATE TABLE burgers (
-'ID' INT AUTO_INCREMENT NOT NULL,
-'burger_name' varchar(100),
-'devoured'  BOOLEAN,
-'date' TIMESTAMP,
+ID INT AUTO_INCREMENT NOT NULL,
+burger_name varchar(100),
+devoured  BOOLEAN,
+date TIMESTAMP,
 PRIMARY KEY (ID)
-)
+);
