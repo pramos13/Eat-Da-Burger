8e51fd3a02c3b293a05b853cb088b32a7ee6caca
Phillip Ramos <phillipramos@Phillips-MacBook-Pro.local>
2016-07-06 16:08:52 -0400

    homework complete

--- burger/db/schema.sql
+++ burger/db/schema.sql
@@ -1,9 +1,10 @@
 CREATE DATABASE burgers_db;
 
 CREATE TABLE burgers (
-ID INT AUTO_INCREMENT NOT NULL,
-burger_name varchar(100),
-devoured  BOOLEAN,
-date TIMESTAMP,
-PRIMARY KEY (ID)
+
+	ID INT AUTO_INCREMENT NOT NULL,
+	burger_name varchar(100),
+	devoured  BOOLEAN,
+	date TIMESTAMP,
+	PRIMARY KEY (ID)
 );
